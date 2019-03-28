23f715b60c8412bdb07f80398b99a7b29157f52c
Kieran Brahney <kieran@supportpal.com>
2019-03-28 11:31:05 +0000

    Fixed #364

--- src/Remote/Validator.php
+++ src/Remote/Validator.php
@@ -165,7 +165,7 @@ class Validator
         $protectedValidator = $this->createProtectedCaller($validator);
 
         foreach ($rules as $i => $rule) {
-            $parsedRule = ValidationRuleParser::parse([$rule]);
+            $parsedRule = ValidationRuleParser::parse($rule);
             if (! $this->isRemoteRule($parsedRule[0])) {
                 unset($rules[$i]);
             }
